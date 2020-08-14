2afb210e864c721e1fb90d85266e043da0d299ef
Kerim <kerimcanislamoglu1@gmail.com>
2020-08-14 21:48:48 +0300

    Fetched data from https://rickandmortyapi.com/api/character/ and rendered to the flat list with a simplified design.

--- App.js
+++ App.js
@@ -1,21 +1,160 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, ScrollView, Image, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window')
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [data, setData] = useState([]);
+  const [firstSeen, setFirstSeen] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character/')
+      .then((response) => response.json())
+      .then((json) => setData(json.results))
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
+  const getFirstSeen = (item) => {
+    fetch(item)
+      .then((response) => response.json())
+      .then((json) => (setFirstSeen(oldArray => [...oldArray, json.name])))
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  function setStatusCircle({ item }) {
+    if (item.status === 'Alive') {
+      return <View style={styles.circleAlive} />
+    } else if (item.status === 'Dead') {
+      return <View style={styles.circleDead} />
+    } else if (item.status === 'unknown') {
+      return <View style={styles.circleUnknown} />
+    }
+  }
+
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <View style={styles.imageView}>
+        <Image style={styles.image} source={{
+          uri: item.image,
+        }} />
+      </View>
+      <View style={styles.textView}>
+        <View style={styles.textGroupView}>
+          <Text style={styles.title}>{item.name}</Text>
+          <View style={styles.circleAndStatusView}>
+            {setStatusCircle({ item })}
+            <Text style={styles.status}> {item.status} - {item.species}</Text>
+          </View>
+
+        </View>
+        <View style={styles.textGroupView}>
+          <Text style={styles.lastKnownLoc}>Last known location:</Text>
+          <Text style={styles.locationName}>{item.location.name}</Text>
+        </View>
+        <View style={styles.textGroupView}>
+          {getFirstSeen(item.episode[0])}
+          <Text style={styles.lastKnownLoc}>First Seen in:</Text>
+          <Text style={styles.locationName}>{firstSeen[item.id - 1]}</Text>
+        </View>
+      </View>
     </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </ScrollView>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#202329',
+  },
+  item: {
+    backgroundColor: '#3C3E44',
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row'
+
+  },
+  title: {
+    fontSize: 16,
+    color: 'white',
+    fontWeight: 'bold'
+
+  },
+  imageView: {
+    flex: 2,
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textView: {
+    flex: 4,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginLeft: 15
+  },
+  image: {
+    width: width * 0.3,
+    height: height * 0.2
+  },
+  textGroupView: {
+    flex: 1,
+    marginVertical: height * 0.01
+  },
+  status: {
+    fontSize: 12,
+    color: 'white',
+
+  },
+  lastKnownLoc: {
+    fontSize: 12,
+    color: '#9E9E9E',
+  },
+  locationName: {
+    color: 'white',
+    fontSize: 16,
+
+  },
+  circleAndStatusView: {
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  circleAlive: {
+    height: 6,
+    width: 6,
+    borderRadius: 3,
+    backgroundColor: '#55cc44',
+   
+  },
+  circleDead: {
+    height: 6,
+    width: 6,
+    borderRadius: 3,
+    backgroundColor: '#ea000c',
+    
+  },
+  circleUnknown: {
+    height: 6,
+    width: 6,
+    borderRadius: 3,
+    backgroundColor: '#9e9e9e',
+  
   },
 });
